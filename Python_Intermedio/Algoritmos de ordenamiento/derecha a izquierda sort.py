23f2f163c45a9e1cf95ad110a626fdf5e6dba9c3
Alejandro Arce <ale_arce1601@hotmail.com>
2026-08-08 14:17:20 -0600

    Adding requested fix

--- Python_Intermedio/Algoritmos de ordenamiento/derecha a izquierda sort.py
+++ Python_Intermedio/Algoritmos de ordenamiento/derecha a izquierda sort.py
@@ -1,10 +1,8 @@
 def bubble_sort_reverse(list_to_sort):
-    for outer_index in range(len(list_to_sort) -1):
-        pass
-
-def bubble_sort_reverse(list_to_sort):
     for outer_index in range(len(list_to_sort) - 1):
-        for index in range(len(list_to_sort) -1, outer_index, -1):
+        has_made_changes = False
+
+        for index in range(len(list_to_sort) - 1, outer_index, -1):
             current_element = list_to_sort[index]
             previous_element = list_to_sort[index - 1]
 
@@ -15,6 +13,7 @@ def bubble_sort_reverse(list_to_sort):
 
         if not has_made_changes:
             return
+
 
 my_list = [18, -11, 68, 6, 32, 53, -2]
 bubble_sort_reverse(my_list)
